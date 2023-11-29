cbfe3561aaa686ed7b095e9498c9844f5dbdff6c
krackatoa0 <149634732+krackatoa0@users.noreply.github.com>
2023-11-29 09:15:12 -0500

    Update plum16.lua

--- lua/colors/plum16.lua
+++ lua/colors/plum16.lua
@@ -1,6 +1,6 @@
 return {
     base00 = 'none', base01 = '#32302f', base02 = '#504945', base03 = '#7f738c',
     base04 = '#bdae93', base05 = '#ddc7a1', base06 = '#ebdbb2', base07 = '#fbf1c7',
-    base08 = '#000000', base09 = '#606a9f', base0A = '#d8a657', base0B = '#ddd1de',
+    base08 = '#9f7fb8', base09 = '#606a9f', base0A = '#d8a657', base0B = '#ddd1de',
     base0C = '#89b482', base0D = '#73609f', base0E = '#88609f', base0F = '#665c54'
 }
